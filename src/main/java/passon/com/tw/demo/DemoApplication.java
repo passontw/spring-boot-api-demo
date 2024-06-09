977656bfa5c5a67b66e0d0b35c21489e034236a1
TomasLin <horsekit1982@gmail.com>
2024-06-09 22:12:47 +0800

    initial project

---
+++ src/main/java/passon/com/tw/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package passon.com.tw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
