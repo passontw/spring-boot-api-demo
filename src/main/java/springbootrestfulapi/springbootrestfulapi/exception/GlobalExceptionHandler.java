5fc41c29a66fd0759f704780281630f716a7682c
Tomas <tomas@PJM-Jun.local>
2024-06-21 18:05:51 +0800

    add custom validate error messages

--- src/main/java/springbootrestfulapi/springbootrestfulapi/exception/GlobalExceptionHandler.java
+++ src/main/java/springbootrestfulapi/springbootrestfulapi/exception/GlobalExceptionHandler.java
@@ -1,15 +1,31 @@
 package springbootrestfulapi.springbootrestfulapi.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private final View error;
+
+    public GlobalExceptionHandler(View error) {
+        this.error = error;
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetail> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest webRequest) {
         ErrorDetail errorDetail = new ErrorDetail(
@@ -44,5 +60,21 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<ErrorDetail>(errorDetail, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        List<ObjectError> errorList = ex.getBindingResult().getAllErrors();
+        errorList.forEach(((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+
+        }));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
