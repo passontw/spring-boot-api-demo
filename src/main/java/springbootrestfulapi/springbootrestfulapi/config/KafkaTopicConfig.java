9e68ebbf759851e007812d2c98dc0c09dada0fba
TomasLin <horsekit1982@gmail.com>
2024-07-08 17:48:40 +0800

    增加 ack mode

--- src/main/java/springbootrestfulapi/springbootrestfulapi/config/KafkaTopicConfig.java
+++ src/main/java/springbootrestfulapi/springbootrestfulapi/config/KafkaTopicConfig.java
@@ -4,8 +4,10 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
 
+@EnableKafka
 @Configurable
 public class KafkaTopicConfig {
 
